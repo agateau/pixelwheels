db53cb8933c556f803b25e39bd0254ecb2185a13
Aurélien Gâteau <mail@agateau.com>
2014-11-21 17:39:07 +0100

    StageScreen: Better behavior on resize

--- core/src/com/greenyetilab/utils/StageScreen.java
+++ core/src/com/greenyetilab/utils/StageScreen.java
@@ -31,4 +31,10 @@ public class StageScreen extends ScreenAdapter {
         mStage.act(delta);
         mStage.draw();
     }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        mViewport.update(width, height, false);
+    }
 }
