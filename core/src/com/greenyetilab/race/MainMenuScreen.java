f9c08838c8fbe7815d26571d4c80ea039a6eae43
Aurélien Gâteau <mail@agateau.com>
2014-11-21 17:00:23 +0100

    Create StageScreen

--- core/src/com/greenyetilab/race/MainMenuScreen.java
+++ core/src/com/greenyetilab/race/MainMenuScreen.java
@@ -1,28 +1,20 @@
 package com.greenyetilab.race;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by aurelien on 21/11/14.
  */
-public class MainMenuScreen extends ScreenAdapter {
+public class MainMenuScreen extends com.greenyetilab.utils.StageScreen {
     private final RaceGame mGame;
-    private Stage mStage;
-    private Viewport mViewport;
 
     public MainMenuScreen(RaceGame game) {
+        super();
         mGame = game;
-        mViewport = new ScreenViewport();
-        mStage = new Stage(mViewport);
 
         Skin skin = game.getAssets().skin;
 
@@ -39,15 +31,7 @@ public class MainMenuScreen extends ScreenAdapter {
             }
         });
 
-        mStage.addActor(button);
-        Gdx.input.setInputProcessor(mStage);
+        getStage().addActor(button);
     }
 
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        mStage.act(delta);
-        mStage.draw();
-    }
 }
---
+++ core/src/com/greenyetilab/utils/StageScreen.java
@@ -0,0 +1,34 @@
+package com.greenyetilab.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by aurelien on 21/11/14.
+ */
+public class StageScreen extends ScreenAdapter {
+    private Stage mStage;
+    private Viewport mViewport;
+
+    public StageScreen() {
+        mViewport = new ScreenViewport();
+        mStage = new Stage(mViewport);
+        Gdx.input.setInputProcessor(mStage);
+    }
+
+    public Stage getStage() {
+        return mStage;
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        mStage.act(delta);
+        mStage.draw();
+    }
+}
