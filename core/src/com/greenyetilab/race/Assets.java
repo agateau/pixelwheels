68c59203418b3414b190e1f49db0c9bd84e36434
Aurélien Gâteau <mail@agateau.com>
2014-11-21 17:39:40 +0100

    Add buttons for all maps

--- core/src/com/greenyetilab/race/Assets.java
+++ core/src/com/greenyetilab/race/Assets.java
@@ -3,6 +3,7 @@ package com.greenyetilab.race;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by aurelien on 21/11/14.
@@ -11,10 +12,20 @@ public class Assets {
     public final Skin skin;
     public final Texture car;
     public final Texture wheel;
+    public Array<String> mapNameList = new Array<String>();
 
     Assets() {
         this.skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
         this.car = new Texture("car.png");
         this.wheel = new Texture("wheel.png");
+
+        mapNameList.addAll(
+                "race.tmx",
+                "round.tmx",
+                "roads.tmx",
+                "clara.tmx",
+                "antonin.tmx",
+                "experiment.tmx"
+        );
     }
 }
--- core/src/com/greenyetilab/race/MainMenuScreen.java
+++ core/src/com/greenyetilab/race/MainMenuScreen.java
@@ -1,9 +1,11 @@
 package com.greenyetilab.race;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.greenyetilab.utils.anchor.Anchor;
 import com.greenyetilab.utils.anchor.AnchorGroup;
@@ -18,24 +20,35 @@ public class MainMenuScreen extends com.greenyetilab.utils.StageScreen {
         super();
         mGame = game;
 
-        Skin skin = game.getAssets().skin;
-
-        final TextButton button = new TextButton("Start", skin, "default");
-
-        button.setWidth(200f);
-        button.setHeight(40f);
-
-        button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                mGame.start("race.tmx");
-            }
-        });
-
         AnchorGroup group = new AnchorGroup();
         group.setFillParent(true);
-        group.addRule(button, Anchor.CENTER, group, Anchor.CENTER);
+
+        Group vGroup = new Group();
+        float w = 0;
+        float y = 0;
+        for(String name: game.getAssets().mapNameList) {
+            TextButton button = createStartButton(name);
+            vGroup.addActor(button);
+            button.setY(y);
+            y += button.getHeight() + 10;
+            w = Math.max(button.getWidth(), w);
+        }
+        vGroup.setSize(w, y - 10);
+
+        group.addRule(vGroup, Anchor.CENTER, group, Anchor.CENTER);
         getStage().addActor(group);
     }
 
+    private TextButton createStartButton(final String name) {
+        Skin skin = mGame.getAssets().skin;
+        TextButton button = new TextButton("Start " + name, skin, "default");
+        button.setSize(200, 40);
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                mGame.start(name);
+            }
+        });
+        return button;
+    }
 }
