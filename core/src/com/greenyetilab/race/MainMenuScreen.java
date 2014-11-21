c35853eb4b364a066b32f77577925f95041f49ec
Aurélien Gâteau <mail@agateau.com>
2014-11-21 19:13:26 +0100

    Nicer layout

--- core/src/com/greenyetilab/race/MainMenuScreen.java
+++ core/src/com/greenyetilab/race/MainMenuScreen.java
@@ -3,6 +3,7 @@ package com.greenyetilab.race;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
@@ -15,10 +16,11 @@ import com.greenyetilab.utils.anchor.AnchorGroup;
  */
 public class MainMenuScreen extends com.greenyetilab.utils.StageScreen {
     private final RaceGame mGame;
+    private static final float PADDING = 20;
 
     public MainMenuScreen(RaceGame game) {
-        super();
         mGame = game;
+        Skin skin = mGame.getAssets().skin;
 
         AnchorGroup group = new AnchorGroup();
         group.setFillParent(true);
@@ -30,24 +32,25 @@ public class MainMenuScreen extends com.greenyetilab.utils.StageScreen {
             TextButton button = createStartButton(mapInfo);
             vGroup.addActor(button);
             button.setY(y);
-            y += button.getHeight() + 10;
+            y += button.getHeight() + PADDING;
             w = Math.max(button.getWidth(), w);
         }
-        vGroup.setSize(w, y - 10);
+        vGroup.setSize(w, y - PADDING);
 
         group.addRule(vGroup, Anchor.CENTER, group, Anchor.CENTER);
+        group.addRule(new Label("Select Race", skin), Anchor.BOTTOM_CENTER, vGroup, Anchor.TOP_CENTER, 0, PADDING);
         getStage().addActor(group);
     }
 
     private TextButton createStartButton(final MapInfo mapInfo) {
         Skin skin = mGame.getAssets().skin;
-        String text = "Start " + mapInfo.getTitle();
+        String text = mapInfo.getTitle();
         float best = mapInfo.getBestTime();
         if (best > 0) {
             text += " (" + StringUtils.formatRaceTime(best) + ")";
         }
         TextButton button = new TextButton(text, skin, "default");
-        button.setSize(200, 40);
+        button.setSize(200, 60);
         button.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
