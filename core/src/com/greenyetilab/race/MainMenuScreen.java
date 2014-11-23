a496ff66f892f6a5a02f1f31c2b1806360399650
Aurélien Gâteau <mail@agateau.com>
2014-11-23 19:44:08 +0100

    unused import--

--- core/src/com/greenyetilab/race/MainMenuScreen.java
+++ core/src/com/greenyetilab/race/MainMenuScreen.java
@@ -1,12 +1,10 @@
 package com.greenyetilab.race;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.greenyetilab.utils.anchor.Anchor;
 import com.greenyetilab.utils.anchor.AnchorGroup;
