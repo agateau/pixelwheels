408e53eabdf6aa3e33fe0a05c64a7f539e5de0f2
Aurélien Gâteau <mail@agateau.com>
2014-12-10 00:04:50 +0100

    Populate debug screen

--- core/src/com/greenyetilab/race/DebugScreen.java
+++ core/src/com/greenyetilab/race/DebugScreen.java
@@ -1,31 +1,62 @@
 package com.greenyetilab.race;
 
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.greenyetilab.utils.anchor.Anchor;
 import com.greenyetilab.utils.anchor.AnchorGroup;
 
 /**
- * Created by aurelien on 09/12/14.
+ * The debug screen
  */
-public class DebugScreen  extends com.greenyetilab.utils.StageScreen {
+public class DebugScreen extends com.greenyetilab.utils.StageScreen {
     private final RaceGame mGame;
 
     public DebugScreen(RaceGame game) {
         mGame = game;
+        Skin skin = mGame.getAssets().skin;
 
         AnchorGroup root = new AnchorGroup();
         root.setFillParent(true);
         getStage().addActor(root);
 
-        TextButton backButton = new TextButton("Back", mGame.getAssets().skin, "default");
+        VerticalGroup vGroup = new VerticalGroup();
+        CheckBox cb;
+        cb = new CheckBox("Box2D Debug", skin);
+        connectCheckBox(cb, "debug/enabled");
+        vGroup.addActor(cb);
+        cb = new CheckBox("Box2D Debug: Draw Velocities", skin);
+        connectCheckBox(cb, "debug/drawVelocities");
+        vGroup.addActor(cb);
+        vGroup.setSize(vGroup.getPrefWidth(), vGroup.getPrefHeight());
+
+        TextButton backButton = new TextButton("Back", skin, "default");
         backButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 mGame.popScreen();
             }
         });
+
+        root.addPositionRule(vGroup, Anchor.TOP_CENTER, root, Anchor.TOP_CENTER);
         root.addPositionRule(backButton, Anchor.BOTTOM_LEFT, root, Anchor.BOTTOM_LEFT);
+    }
+
+    private void connectCheckBox(final CheckBox checkBox, final String key) {
+        final Preferences prefs = RaceGame.getPreferences();
+        checkBox.setChecked(prefs.getBoolean(key, false));
+        checkBox.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                prefs.putBoolean(key, checkBox.isChecked());
+                prefs.flush();
+            }
+        });
     }
 }
