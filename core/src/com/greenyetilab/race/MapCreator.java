d0a44cfb7a72bb3cb219b1d09084bffc7a906d58
Aurélien Gâteau <mail@agateau.com>
2014-12-10 10:47:46 +0100

    MapCreator: Add support for RectangularMapObject

--- core/src/com/greenyetilab/race/MapCreator.java
+++ core/src/com/greenyetilab/race/MapCreator.java
@@ -1,11 +1,16 @@
 package com.greenyetilab.race;
 
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSets;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
+import com.greenyetilab.utils.log.NLog;
 
 /**
  * Create a large tiled map from several maps
@@ -62,20 +67,36 @@ public class MapCreator {
         // Create layers
         MapLayers layers = dstMap.getLayers();
         for (int layerIdx = 0; layerIdx < layerCount; ++layerIdx) {
-            TiledMapTileLayer dstLayer = new TiledMapTileLayer(mapWidth, mapHeight, (int)tileWidth, (int)tileHeight);
-            dstLayer.setName(referenceMap.getLayers().get(layerIdx).getName());
-            layers.add(dstLayer);
-            int startY = 0;
-            for (TiledMap srcMap : mapSequence) {
-                TiledMapTileLayer srcLayer = (TiledMapTileLayer)srcMap.getLayers().get(layerIdx);
-                copyMapLayer(dstMap, dstLayer, srcLayer, startY);
-                startY += srcLayer.getHeight();
+            MapLayer referenceLayer = referenceMap.getLayers().get(layerIdx);
+            boolean isTileLayer = referenceLayer instanceof TiledMapTileLayer;
+            MapLayer dstLayer;
+            if (isTileLayer) {
+                dstLayer = new TiledMapTileLayer(mapWidth, mapHeight, (int) tileWidth, (int) tileHeight);
+            } else {
+                dstLayer = new MapLayer();
             }
+            dstLayer.setName(referenceLayer.getName());
+            layers.add(dstLayer);
+        }
+
+        // Fill layers
+        int startTY = 0;
+        for (TiledMap srcMap : mapSequence) {
+            for (int layerIdx = 0; layerIdx < layerCount; ++layerIdx) {
+                MapLayer srcLayer = srcMap.getLayers().get(layerIdx);
+                MapLayer dstLayer = dstMap.getLayers().get(layerIdx);
+                if (srcLayer instanceof TiledMapTileLayer) {
+                    copyTileLayer(dstMap, (TiledMapTileLayer) dstLayer, (TiledMapTileLayer) srcLayer, startTY);
+                } else {
+                    copyLayer(dstLayer, srcLayer, startTY * tileHeight);
+                }
+            }
+            startTY += ((TiledMapTileLayer)srcMap.getLayers().get(0)).getHeight();
         }
         return dstMap;
     }
 
-    private static void copyMapLayer(TiledMap dstMap, TiledMapTileLayer dstLayer, TiledMapTileLayer srcLayer, int startY) {
+    private static void copyTileLayer(TiledMap dstMap, TiledMapTileLayer dstLayer, TiledMapTileLayer srcLayer, int startTY) {
         TiledMapTileSets dstTileSets = dstMap.getTileSets();
         for (int ty = 0; ty < srcLayer.getHeight(); ++ty) {
             for (int tx = 0; tx < srcLayer.getWidth(); ++tx) {
@@ -89,7 +110,27 @@ public class MapCreator {
                 dstCell.setFlipHorizontally(srcCell.getFlipHorizontally());
                 dstCell.setFlipVertically(srcCell.getFlipVertically());
                 dstCell.setRotation(srcCell.getRotation());
-                dstLayer.setCell(tx, startY + ty, dstCell);
+                dstLayer.setCell(tx, startTY + ty, dstCell);
+            }
+        }
+    }
+
+    private static void copyLayer(MapLayer dstLayer, MapLayer srcLayer, float startY) {
+        for (MapObject srcObject : srcLayer.getObjects()) {
+            MapObject dstObject = null;
+            if (srcObject instanceof RectangleMapObject) {
+                Rectangle rect = ((RectangleMapObject) srcObject).getRectangle();
+                dstObject = new RectangleMapObject(rect.x, startY + rect.y, rect.width, rect.height);
+            } else {
+                NLog.e("Map objects of type %s are not supported yet", srcObject.getClass());
+            }
+            if (dstObject != null) {
+                dstObject.setName(srcObject.getName());
+                dstObject.setColor(srcObject.getColor());
+                dstObject.setVisible(srcObject.isVisible());
+                dstObject.setOpacity(srcObject.getOpacity());
+                dstObject.getProperties().putAll(srcObject.getProperties());
+                dstLayer.getObjects().add(dstObject);
             }
         }
     }
