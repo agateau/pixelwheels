629c170f72806e3396383bb3d1ae92358e18326c
Aurélien Gâteau <mail@agateau.com>
2014-12-20 00:33:42 +0100

    Increase accelerometer sensitivity

--- core/src/com/greenyetilab/race/AccelerometerInputHandler.java
+++ core/src/com/greenyetilab/race/AccelerometerInputHandler.java
@@ -7,16 +7,14 @@ import com.badlogic.gdx.math.MathUtils;
  * Handles game input using the accelerometer
  */
 public class AccelerometerInputHandler implements GameInputHandler {
-    private static final float ACCELEROMETER_DEAD_ZONE = 0;
-    private static final float MAX_ACCELEROMETER = 12 + ACCELEROMETER_DEAD_ZONE;
+    private static final float MAX_ACCELEROMETER = 10;
 
     @Override
     public void updateGameInput(GameInput input) {
         input.braking = false;
         input.accelerating = true;
         float angle = -Gdx.input.getAccelerometerY();
-        if (Math.abs(angle) >= ACCELEROMETER_DEAD_ZONE) {
-            input.direction = MathUtils.clamp(angle - ACCELEROMETER_DEAD_ZONE, -MAX_ACCELEROMETER, MAX_ACCELEROMETER) / MAX_ACCELEROMETER;
+        input.direction = MathUtils.clamp(angle, -MAX_ACCELEROMETER, MAX_ACCELEROMETER) / MAX_ACCELEROMETER;
         }
     }
 }
