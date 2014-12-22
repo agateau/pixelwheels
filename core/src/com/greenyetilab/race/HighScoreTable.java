3eb4fc46e72ad62e9e9663bf6261d224774f3bd2
Aurélien Gâteau <mail@agateau.com>
2014-12-22 12:11:19 +0100

    Load and save high scores

--- core/src/com/greenyetilab/race/HighScoreTable.java
+++ core/src/com/greenyetilab/race/HighScoreTable.java
@@ -2,6 +2,12 @@ package com.greenyetilab.race;
 
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.IntArray;
+import com.badlogic.gdx.utils.XmlReader;
+import com.badlogic.gdx.utils.XmlWriter;
+import com.greenyetilab.utils.FileUtils;
+import com.greenyetilab.utils.log.NLog;
+
+import java.io.IOException;
 
 /**
  * Handle the player high scores
@@ -45,12 +51,47 @@ public class HighScoreTable {
         save();
     }
 
-    private void load() {
-
+    public void load() {
+        if (mFile.exists()) {
+            load(FileUtils.parseXml(mFile));
+        }
     }
 
-    private void save() {
+    public void load(XmlReader.Element root) {
+        if (root == null) {
+            NLog.e("called with a null object, not loading anything");
+            return;
+        }
+        int size = Math.min(root.getChildCount(), mScores.size);
+        int idx = 0;
+        for (; idx < size; ++idx) {
+            XmlReader.Element element = root.getChild(idx);
+            int score = element.getIntAttribute("value");
+            mScores.set(idx, score);
+        }
+        // Fill the array with zeros, just in case
+        for (; idx < mScores.size; ++idx) {
+            mScores.set(idx, 0);
+        }
+    }
 
+    public void save() {
+        save(new XmlWriter(mFile.writer(false)));
+    }
+
+    public void save(XmlWriter writer) {
+        try {
+            XmlWriter root = writer.element(("highscores"));
+            for (int i = 0; i < mScores.size; ++i) {
+                int score = mScores.get(i);
+                root.element("highscore")
+                        .attribute("value", score)
+                        .pop();
+            }
+            writer.close();
+        } catch (IOException e) {
+            NLog.e("Failed to save highscores. Exception: %s", e);
+        }
     }
 
     private void fill() {
