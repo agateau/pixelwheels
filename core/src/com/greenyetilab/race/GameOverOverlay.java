a2863d5ce567049345827c7a6fd0c1e44fa75694
Aurélien Gâteau <mail@agateau.com>
2014-12-22 12:38:58 +0100

    Create a custom actor for high score pane

--- core/src/com/greenyetilab/race/GameOverOverlay.java
+++ core/src/com/greenyetilab/race/GameOverOverlay.java
@@ -1,29 +1,17 @@
 package com.greenyetilab.race;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.ui.Cell;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.IntArray;
+import com.badlogic.gdx.utils.XmlReader;
 import com.greenyetilab.utils.FileUtils;
 import com.greenyetilab.utils.RefreshHelper;
 import com.greenyetilab.utils.UiBuilder;
-import com.greenyetilab.utils.log.NLog;
-
-import javax.swing.Scrollable;
 
 /**
  * Appears on top of RaceGameScreen when player has lost
  */
 public class GameOverOverlay extends Overlay {
     private final RaceGame mGame;
-    private Cell mNewHighScoreCell = null;
-    private ScrollPane mScrollPane;
 
     public GameOverOverlay(RaceGame game, final int newHighScoreIndex) {
         super(game.getAssets().dot);
@@ -37,18 +25,17 @@ public class GameOverOverlay extends Overlay {
         setContent(createContent(newHighScoreIndex));
     }
 
-    @Override
-    public void layout() {
-        super.layout();
-        if (mNewHighScoreCell != null) {
-            float y = mNewHighScoreCell.getActorY();
-            float height = mNewHighScoreCell.getActorHeight();
-            mScrollPane.scrollToCenter(0, y, mScrollPane.getWidth(), height);
-        }
-    }
-
     private Actor createContent(int newHighScoreIndex) {
-        UiBuilder builder = new UiBuilder(mGame.getAssets().atlas, mGame.getAssets().skin);
+        UiBuilder builder = new UiBuilder(mGame.getAssets().atlas, mGame.getAssets().skin) {
+            @Override
+            protected Actor createActorForElement(XmlReader.Element element) {
+                if (element.getName().equals("HighScorePane")) {
+                    return new HighScorePane();
+                } else {
+                    return super.createActorForElement(element);
+                }
+            }
+        };
         Actor content = builder.build(FileUtils.assets("screens/gameoveroverlay.gdxui"));
         builder.getActor("restartButton").addListener(new ChangeListener() {
             @Override
@@ -62,26 +49,8 @@ public class GameOverOverlay extends Overlay {
                 mGame.showMainMenu();
             }
         });
-        mScrollPane = builder.getActor("scoreScrollPane");
-        Actor actor = createScoreTable(newHighScoreIndex);
-        actor.setWidth(mScrollPane.getWidth());
-        mScrollPane.setWidget(actor);
+        HighScorePane scorePane = builder.getActor("scoreScrollPane");
+        scorePane.init(mGame.getAssets().skin, mGame.getHighScoreTable(), newHighScoreIndex);
         return content;
-    }
-
-    private Actor createScoreTable(int newHighScoreIndex) {
-        Skin skin = mGame.getAssets().skin;
-        Table table = new Table(skin);
-        IntArray scores = mGame.getHighScoreTable().getScores();
-        for (int idx = 0; idx < scores.size; ++idx) {
-            String style = idx == newHighScoreIndex ? "newHighScore" : "highScore";
-            Cell cell = table.add(String.format("%d.", idx + 1), style).right();
-            if (idx == newHighScoreIndex) {
-                mNewHighScoreCell = cell;
-            }
-            table.add(String.valueOf(scores.get(idx)), style).expandX().right();
-            table.row();
-        }
-        return table;
     }
 }
---
+++ core/src/com/greenyetilab/race/HighScorePane.java
@@ -0,0 +1,47 @@
+package com.greenyetilab.race;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.IntArray;
+
+/**
+ * Display the high score table
+ */
+class HighScorePane extends ScrollPane {
+    private Cell mNewHighScoreCell = null;
+
+    public HighScorePane() {
+        super(null);
+    }
+
+    public void init(Skin skin, HighScoreTable highScoreTable) {
+        init(skin, highScoreTable, -1);
+    }
+
+    public void init(Skin skin, HighScoreTable highScoreTable, int newHighScoreIndex) {
+        Table table = new Table(skin);
+        IntArray scores = highScoreTable.getScores();
+        for (int idx = 0; idx < scores.size; ++idx) {
+            String style = idx == newHighScoreIndex ? "newHighScore" : "highScore";
+            Cell cell = table.add(String.format("%d.", idx + 1), style).right();
+            if (idx == newHighScoreIndex) {
+                mNewHighScoreCell = cell;
+            }
+            table.add(String.valueOf(scores.get(idx)), style).expandX().right();
+            table.row();
+        }
+        setWidget(table);
+    }
+
+    @Override
+    public void layout() {
+        super.layout();
+        if (mNewHighScoreCell != null) {
+            float y = mNewHighScoreCell.getActorY();
+            float height = mNewHighScoreCell.getActorHeight();
+            scrollToCenter(0, y, getWidth(), height);
+        }
+    }
+}
