6f52b3c9a035efa5f17cc9b4d05c75a30db3b596
Aurélien Gâteau <mail@agateau.com>
2015-03-24 23:49:04 +0100

    Make NMessageBus non-static only

--- core/src/com/greenyetilab/race/NMessageBus.java
+++ core/src/com/greenyetilab/race/NMessageBus.java
@@ -8,13 +8,13 @@ import java.util.HashMap;
  * Super simple message bus system
  */
 public class NMessageBus {
-    private static final HashMap<String, Array<Handler>> mHandlerForChannel = new HashMap<String, Array<Handler>>();
+    private final HashMap<String, Array<Handler>> mHandlerForChannel = new HashMap<String, Array<Handler>>();
 
     public interface Handler {
         public void handle(String channel, Object data);
     }
 
-    public static void register(String channel, Handler handler) {
+    public void register(String channel, Handler handler) {
         Array<Handler> array = mHandlerForChannel.get(channel);
         if (array == null) {
             array = new Array<Handler>();
@@ -23,7 +23,11 @@ public class NMessageBus {
         array.add(handler);
     }
 
-    public static void post(String channel, Object data) {
+    public void post(String channel) {
+        post(channel, null);
+    }
+
+    public void post(String channel, Object data) {
         Array<Handler> array = mHandlerForChannel.get(channel);
         if (array == null) {
             return;
