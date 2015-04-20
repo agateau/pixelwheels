63f4141b0f10bebed4047eaf03aea02c3d815b26
Aurélien Gâteau <mail@agateau.com>
2015-04-20 23:46:32 +0200

    More skidmarks

--- core/src/com/greenyetilab/race/GamePlay.java
+++ core/src/com/greenyetilab/race/GamePlay.java
@@ -11,7 +11,7 @@ public class GamePlay {
     public static int racerCount = 8;
     public static int maxDrivingForce = 100;
     public static int maxLateralImpulse = 10;
-    public static int maxSkidmarks = 20;
+    public static int maxSkidmarks = 200;
     public static int lowSpeedMaxSteer = 40;
     public static int highSpeedMaxSteer = 10;
     public static int vehicleDensity = 9;
