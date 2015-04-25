2de869b527361ed809f454e20d07030006518295
Aurélien Gâteau <mail@agateau.com>
2015-04-25 18:50:12 +0200

    A bit faster, a bit slippier

--- core/src/com/greenyetilab/race/GamePlay.java
+++ core/src/com/greenyetilab/race/GamePlay.java
@@ -9,8 +9,8 @@ import com.greenyetilab.utils.Introspector;
  */
 public class GamePlay {
     public static int racerCount = 8;
-    public static int maxDrivingForce = 100;
-    public static int maxLateralImpulse = 10;
+    public static int maxDrivingForce = 110;
+    public static int maxLateralImpulse = 9;
     public static int maxSkidmarks = 200;
     public static int lowSpeedMaxSteer = 40;
     public static int highSpeedMaxSteer = 10;
