ec8c0dfd5e3a358c7a0be81ec3f175420d80f48e
Aurélien Gâteau <agateau@genymobile.com>
2015-06-12 08:22:43 +0200

    Move Hud to its own file

---
+++ core/src/com/greenyetilab/tinywheels/Hud.java
@@ -0,0 +1,104 @@
+package com.greenyetilab.tinywheels;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
+import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.utils.StringBuilder;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import java.util.Map;
+
+/**
+ * Hud showing player info during race
+ */
+class Hud {
+    private final PerformanceCounters mPerformanceCounters;
+    private Stage mHudStage;
+    private ScreenViewport mHudViewport;
+    private WidgetGroup mHud;
+    private Label mLapLabel;
+    private Label mSpeedLabel;
+    private Label mDebugLabel;
+
+    public Hud(Assets assets, Batch batch, PerformanceCounters performanceCounters) {
+        mPerformanceCounters = performanceCounters;
+        mHudViewport = new ScreenViewport();
+        mHudStage = new Stage(mHudViewport, batch);
+        Gdx.input.setInputProcessor(mHudStage);
+
+        Skin skin = assets.skin;
+        mHud = new WidgetGroup();
+
+        mLapLabel = new Label("0:00.0", skin);
+        mSpeedLabel = new Label("0", skin);
+        mHud.addActor(mLapLabel);
+        mHud.addActor(mSpeedLabel);
+        mHud.setHeight(mLapLabel.getHeight());
+
+        if (TheGame.getPreferences().getBoolean("debug/showDebugHud", false)
+                && mPerformanceCounters != null) {
+            mDebugLabel = new Label("D", skin, "small");
+            mHudStage.addActor(mDebugLabel);
+        }
+        mHudStage.addActor(mHud);
+    }
+
+    public Stage getStage() {
+        return mHudStage;
+    }
+
+    public void act(float delta) {
+        mHudStage.act(delta);
+    }
+
+    public void draw() {
+        mHudStage.draw();
+    }
+
+    public void resize(int width, int height) {
+        mHudViewport.update(width, height, true);
+    }
+
+    private static com.badlogic.gdx.utils.StringBuilder sDebugSB = new StringBuilder();
+
+    public void updateHud(GameWorld gameWorld, int playerId) { // FIXME Make gameWorld and playerId members and call this from act()
+        int lapCount = Math.max(gameWorld.getPlayerRacer().getLapPositionComponent().getLapCount(), 1);
+        int totalLapCount = gameWorld.getMapInfo().getTotalLapCount();
+        int rank = gameWorld.getPlayerRank();
+        mLapLabel.setText(String.format("Lap: %d/%d Rank: %d", lapCount, totalLapCount, rank));
+        mLapLabel.setPosition(5, 0);
+
+        Vehicle vehicle = gameWorld.getPlayerVehicle(playerId);
+        mSpeedLabel.setText(StringUtils.formatSpeed(vehicle.getSpeed()));
+        mSpeedLabel.setPosition(mHudViewport.getScreenWidth() - mSpeedLabel.getPrefWidth() - 5, 0);
+
+        mHud.setPosition(0, mHudViewport.getScreenHeight() - mHud.getHeight() - 5);
+
+        if (mDebugLabel != null) {
+            sDebugSB.setLength(0);
+            sDebugSB.append("objCount: ").append(gameWorld.getActiveGameObjects().size).append('\n');
+            sDebugSB.append("FPS: ").append(Gdx.graphics.getFramesPerSecond()).append('\n');
+            for (PerformanceCounter counter : mPerformanceCounters.counters) {
+                sDebugSB.append(counter.name).append(": ")
+                        .append(String.valueOf((int) (counter.time.value * 1000)))
+                        .append(" | ")
+                        .append(String.valueOf((int) (counter.load.value * 100)))
+                        .append("%\n")
+                ;
+            }
+            for (Map.Entry<String, String> entry : DebugStringMap.getMap().entrySet()) {
+                sDebugSB.append(entry.getKey())
+                        .append(": ")
+                        .append(entry.getValue())
+                        .append("\n");
+            }
+            mDebugLabel.setText(sDebugSB);
+            mDebugLabel.setPosition(0, mHud.getY() - mDebugLabel.getPrefHeight());
+        }
+    }
+
+}
--- core/src/com/greenyetilab/tinywheels/RaceScreen.java
+++ core/src/com/greenyetilab/tinywheels/RaceScreen.java
@@ -10,16 +10,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.PerformanceCounter;
 import com.badlogic.gdx.utils.PerformanceCounters;
-import com.badlogic.gdx.utils.StringBuilder;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-
-import java.util.Map;
 
 public class RaceScreen extends ScreenAdapter {
     private final TheGame mGame;
@@ -29,88 +22,6 @@ public class RaceScreen extends ScreenAdapter {
 
     private Array<GameRenderer> mGameRenderers = new Array<GameRenderer>();
 
-    private static class Hud {
-        private final PerformanceCounters mPerformanceCounters;
-        private Stage mHudStage;
-        private ScreenViewport mHudViewport;
-        private WidgetGroup mHud;
-        private Label mLapLabel;
-        private Label mSpeedLabel;
-        private Label mDebugLabel;
-
-        public Hud(Assets assets, Batch batch, PerformanceCounters performanceCounters) {
-            mPerformanceCounters = performanceCounters;
-            mHudViewport = new ScreenViewport();
-            mHudStage = new Stage(mHudViewport, batch);
-            Gdx.input.setInputProcessor(mHudStage);
-
-            Skin skin = assets.skin;
-            mHud = new WidgetGroup();
-
-            mLapLabel = new Label("0:00.0", skin);
-            mSpeedLabel = new Label("0", skin);
-            mHud.addActor(mLapLabel);
-            mHud.addActor(mSpeedLabel);
-            mHud.setHeight(mLapLabel.getHeight());
-
-            if (TheGame.getPreferences().getBoolean("debug/showDebugHud", false)
-                    && mPerformanceCounters != null) {
-                mDebugLabel = new Label("D", skin, "small");
-                mHudStage.addActor(mDebugLabel);
-            }
-            mHudStage.addActor(mHud);
-        }
-
-        public void act(float delta) {
-            mHudStage.act(delta);
-        }
-
-        public void draw() {
-            mHudStage.draw();
-        }
-
-        public void resize(int width, int height) {
-            mHudViewport.update(width, height, true);
-        }
-
-        private static StringBuilder sDebugSB = new StringBuilder();
-        private void updateHud(GameWorld gameWorld, int playerId) {
-            int lapCount = Math.max(gameWorld.getPlayerRacer().getLapPositionComponent().getLapCount(), 1);
-            int totalLapCount = gameWorld.getMapInfo().getTotalLapCount();
-            int rank = gameWorld.getPlayerRank();
-            mLapLabel.setText(String.format("Lap: %d/%d Rank: %d", lapCount, totalLapCount, rank));
-            mLapLabel.setPosition(5, 0);
-
-            Vehicle vehicle = gameWorld.getPlayerVehicle(playerId);
-            mSpeedLabel.setText(StringUtils.formatSpeed(vehicle.getSpeed()));
-            mSpeedLabel.setPosition(mHudViewport.getScreenWidth() - mSpeedLabel.getPrefWidth() - 5, 0);
-
-            mHud.setPosition(0, mHudViewport.getScreenHeight() - mHud.getHeight() - 5);
-
-            if (mDebugLabel != null) {
-                sDebugSB.setLength(0);
-                sDebugSB.append("objCount: ").append(gameWorld.getActiveGameObjects().size).append('\n');
-                sDebugSB.append("FPS: ").append(Gdx.graphics.getFramesPerSecond()).append('\n');
-                for (PerformanceCounter counter : mPerformanceCounters.counters) {
-                    sDebugSB.append(counter.name).append(": ")
-                            .append(String.valueOf((int)(counter.time.value * 1000)))
-                            .append(" | ")
-                            .append(String.valueOf((int)(counter.load.value * 100)))
-                            .append("%\n")
-                    ;
-                }
-                for (Map.Entry<String, String> entry : DebugStringMap.getMap().entrySet()) {
-                    sDebugSB.append(entry.getKey())
-                            .append(": ")
-                            .append(entry.getValue())
-                            .append("\n");
-                }
-                mDebugLabel.setText(sDebugSB);
-                mDebugLabel.setPosition(0, mHud.getY() - mDebugLabel.getPrefHeight());
-            }
-        }
-
-    }
     private Hud mHud;
 
     private final PerformanceCounters mPerformanceCounters = new PerformanceCounters();
@@ -134,7 +45,7 @@ public class RaceScreen extends ScreenAdapter {
 
         @Override
         public Stage getStage() {
-            return mHud.mHudStage;
+            return mHud.getStage();
         }
     };
 
@@ -207,7 +118,7 @@ public class RaceScreen extends ScreenAdapter {
     }
 
     private void showFinishedOverlay() {
-        mHud.mHudStage.addActor(new FinishedOverlay(mGame, mGameWorld.getRacers(), mGameWorld.getPlayerRacer()));
+        mHud.getStage().addActor(new FinishedOverlay(mGame, mGameWorld.getRacers(), mGameWorld.getPlayerRacer()));
     }
 
     @Override
