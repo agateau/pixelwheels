6f1f9e4f2a55b9128e1ab81d91db21090f1dd727
Aurélien Gâteau <aurelien@trinity>
2015-08-08 18:41:38 +0200

    Stupid-typo--

--- core/src/com/greenyetilab/tinywheels/GameInputHandlerFactories.java
+++ core/src/com/greenyetilab/tinywheels/GameInputHandlerFactories.java
@@ -38,5 +38,6 @@ public class GameInputHandlerFactories {
         }
         if (Gdx.input.isPeripheralAvailable(Input.Peripheral.MultitouchScreen)) {
             mFactories.add(new TouchInputHandler.Factory());
+        }
     }
 }
