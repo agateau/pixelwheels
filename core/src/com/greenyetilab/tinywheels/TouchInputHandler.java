158f73c1533fe5edc97c1c4bfeeb1378df24d888
Aurélien Gâteau <mail@agateau.com>
2015-09-02 19:27:04 +0200

    Create a ClickArea to create larger touch targets for hud buttons

--- core/src/com/greenyetilab/tinywheels/TouchInputHandler.java
+++ core/src/com/greenyetilab/tinywheels/TouchInputHandler.java
@@ -62,9 +62,22 @@ public class TouchInputHandler implements GameInputHandler {
         mBonusButton = new HudButton(assets, hud, "action");
 
         AnchorGroup root = hud.getRoot();
-        root.addPositionRule(mLeftButton, Anchor.BOTTOM_LEFT, root, Anchor.BOTTOM_LEFT);
-        root.addPositionRule(mRightButton, Anchor.BOTTOM_LEFT, mLeftButton, Anchor.BOTTOM_RIGHT, 12, 0);
-        root.addPositionRule(mBrakeButton, Anchor.BOTTOM_RIGHT, root, Anchor.BOTTOM_RIGHT);
+
+        ClickArea leftArea = new ClickArea(mLeftButton);
+        root.addPositionRule(leftArea, Anchor.BOTTOM_LEFT, root, Anchor.BOTTOM_LEFT);
+        root.addSizeRule(leftArea, root, 0.2f, 0.5f);
+        root.addPositionRule(mLeftButton, Anchor.BOTTOM_CENTER, leftArea, Anchor.BOTTOM_CENTER);
+
+        ClickArea rightArea = new ClickArea(mRightButton);
+        root.addPositionRule(rightArea, Anchor.BOTTOM_LEFT, leftArea, Anchor.BOTTOM_RIGHT);
+        root.addSizeRule(rightArea, root, 0.2f, 0.5f);
+        root.addPositionRule(mRightButton, Anchor.BOTTOM_CENTER, rightArea, Anchor.BOTTOM_CENTER);
+
+        ClickArea brakeArea = new ClickArea(mBrakeButton);
+        root.addSizeRule(brakeArea, root, 0.2f, 0.5f);
+        root.addPositionRule(brakeArea, Anchor.BOTTOM_RIGHT, root, Anchor.BOTTOM_RIGHT);
+        root.addPositionRule(mBrakeButton, Anchor.BOTTOM_CENTER, brakeArea, Anchor.BOTTOM_CENTER);
+
         root.addPositionRule(mBonusButton, Anchor.BOTTOM_RIGHT, mBrakeButton, Anchor.TOP_RIGHT);
     }
 
