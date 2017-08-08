426971ef03743592e67240e18ca74482b8c72d79
Aurélien Gâteau <mail@agateau.com>
2017-08-08 08:19:05 +0200

    Clean up VehicleSelector

--- core/src/com/agateau/tinywheels/VehicleSelector.java
+++ core/src/com/agateau/tinywheels/VehicleSelector.java
@@ -2,29 +2,24 @@ package com.agateau.tinywheels;
 
 import com.agateau.ui.GridMenuItem;
 import com.agateau.ui.Menu;
-import com.agateau.ui.TextureRegionItemRenderer;
 import com.agateau.ui.TextureRegionItemRendererAdapter;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
 
 /**
- * An actor to select a vehicle
+ * A menu item to select a vehicle
  */
 public class VehicleSelector extends GridMenuItem<VehicleDef> {
     private Assets mAssets;
 
-    public VehicleSelector(Menu menu) {
-        super(menu);
-    }
-
     private class Renderer extends TextureRegionItemRendererAdapter<VehicleDef> {
-        private final TextureRegionItemRendererAdapter mRenderer = new TextureRegionItemRenderer();
-
         @Override
         protected TextureRegion getItemRegion(VehicleDef vehicleDef) {
             return mAssets.findRegion("vehicles/" + vehicleDef.mainImage);
         }
+    }
+
+    public VehicleSelector(Menu menu) {
+        super(menu);
     }
 
     public void init(Assets assets) {
