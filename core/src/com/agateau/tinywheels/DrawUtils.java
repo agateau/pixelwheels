d0b44e78b6fe6b8d44820a49abc5fb7e2196342c
Aurélien Gâteau <mail@agateau.com>
2017-08-20 15:42:18 +0200

    Add helper methods to draw crosses

--- core/src/com/agateau/tinywheels/DrawUtils.java
+++ core/src/com/agateau/tinywheels/DrawUtils.java
@@ -3,6 +3,7 @@ package com.agateau.tinywheels;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -50,5 +51,14 @@ public class DrawUtils {
                 w, h, // size
                 1, 1, // scale
                 angle);
+    }
+
+    public static void drawCross(ShapeRenderer renderer, Vector2 pos, float radius) {
+        drawCross(renderer, pos.x, pos.y, radius);
+    }
+
+    public static void drawCross(ShapeRenderer renderer, float x, float y, float radius) {
+        renderer.line(x - radius, y, x + radius, y);
+        renderer.line(x, y - radius, x, y + radius);
     }
 }
