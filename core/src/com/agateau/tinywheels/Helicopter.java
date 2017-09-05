4aac8a2e5c0c1308a134d121ebe805f8d7f81f35
Aurélien Gâteau <mail@agateau.com>
2017-09-05 08:11:51 +0200

    Fix rendering of helicopter shadow

--- core/src/com/agateau/tinywheels/Helicopter.java
+++ core/src/com/agateau/tinywheels/Helicopter.java
@@ -1,9 +1,16 @@
 package com.agateau.tinywheels;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.Pool;
@@ -18,10 +25,8 @@ import static com.agateau.tinywheels.DrawUtils.SHADOW_ALPHA;
 public class Helicopter extends GameObjectAdapter implements Pool.Poolable, Disposable {
     private static final float ARRIVING_DURATION = 2;
     private static final float SHADOW_OFFSET = 80;
-    private static final Vector2 BODY_CENTER = new Vector2(
-            Constants.UNIT_FOR_PIXEL * 30,
-            Constants.UNIT_FOR_PIXEL * (111 - 35));
-    private static final float PROPELLER_SPEED = 720;
+    private static final Vector2 BODY_CENTER = new Vector2(30, (111 - 35));
+    private static final float PROPELLER_SPEED = -720;
 
     private enum State {
         ARRIVING,
@@ -30,6 +35,8 @@ public class Helicopter extends GameObjectAdapter implements Pool.Poolable, Disp
     }
     private static final ReflectionPool<Helicopter> sPool = new ReflectionPool<Helicopter>(Helicopter.class);
 
+    private FrameBuffer mFrameBuffer;
+    private SpriteBatch mFrameBufferBatch;
     private TextureRegion mBodyRegion;
     private TextureRegion mPropellerRegion;
     private TextureRegion mPropellerTopRegion;
@@ -56,6 +63,14 @@ public class Helicopter extends GameObjectAdapter implements Pool.Poolable, Disp
         object.mLeavePosition.set(vehiclePosition.x, mapHeight);
         object.mTime = 0;
         object.mState = State.ARRIVING;
+
+        if (object.mFrameBuffer == null) {
+            int bufferWidth = object.mPropellerRegion.getRegionWidth();
+            int bufferHeight = object.mPropellerRegion.getRegionHeight() / 2 + (int)BODY_CENTER.y;
+            object.mFrameBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, bufferWidth, bufferHeight, false /* hasDepth */);
+            object.mFrameBufferBatch = new SpriteBatch();
+            object.mFrameBufferBatch.setProjectionMatrix(new Matrix4().setToOrtho2D(0, 0, bufferWidth, bufferHeight));
+        }
 
         return object;
     }
@@ -127,46 +142,69 @@ public class Helicopter extends GameObjectAdapter implements Pool.Poolable, Disp
     @Override
     public void draw(Batch batch, int zIndex) {
         if (zIndex == Constants.Z_SHADOWS) {
+            batch.end();
+            updateFrameBuffer();
+            batch.begin();
             Color old = batch.getColor();
             batch.setColor(0, 0, 0, SHADOW_ALPHA);
             float offset = SHADOW_OFFSET * Constants.UNIT_FOR_PIXEL;
-            drawHelicopter(batch, offset);
+            drawFrameBuffer(batch, offset);
             batch.setColor(old);
         } else if (zIndex == Constants.Z_FLYING) {
-            drawHelicopter(batch, 0);
+            drawFrameBuffer(batch, 0);
         }
     }
 
-    private void drawHelicopter(Batch batch, float offset) {
-        final float U = Constants.UNIT_FOR_PIXEL;
-        final float x = mPosition.x + offset;
-        final float y = mPosition.y + offset;
-        final float w = U * mBodyRegion.getRegionWidth();
-        final float h = U * mBodyRegion.getRegionHeight();
+    @SuppressWarnings("SuspiciousNameCombination")
+    private void updateFrameBuffer() {
+        final float w = mBodyRegion.getRegionWidth();
+        final float h = mBodyRegion.getRegionHeight();
 
-        float propellerW = U * mPropellerRegion.getRegionWidth();
-        float propellerH = U * mPropellerRegion.getRegionHeight();
+        float propellerW = mPropellerRegion.getRegionWidth();
+        float propellerH = mPropellerRegion.getRegionHeight();
 
-        float propellerTopW = U * mPropellerTopRegion.getRegionWidth();
-        float propellerTopH = U * mPropellerTopRegion.getRegionHeight();
+        float propellerTopW = mPropellerTopRegion.getRegionWidth();
+        float propellerTopH = mPropellerTopRegion.getRegionHeight();
 
-        batch.draw(
+        mFrameBuffer.begin();
+        mFrameBufferBatch.begin();
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        mFrameBufferBatch.draw(
                 mBodyRegion,
-                x - BODY_CENTER.x, y - BODY_CENTER.y,
+                propellerW / 2 - BODY_CENTER.x, 0,
                 w, h);
 
-        batch.draw(
+        mFrameBufferBatch.draw(
                 mPropellerRegion,
-                x - propellerW / 2, y - propellerH / 2, // position
+                0, BODY_CENTER.y - propellerH / 2, // position
                 propellerW / 2, propellerH / 2, // origin
                 propellerW, propellerH, // size
                 1, 1, // scale
                 (mTime * PROPELLER_SPEED) % 360);
 
-        batch.draw(
+        mFrameBufferBatch.draw(
                 mPropellerTopRegion,
-                x - propellerTopW / 2, y - propellerTopH / 2,
+                propellerW / 2 - propellerTopW / 2, BODY_CENTER.y - propellerTopH / 2,
                 propellerTopW, propellerTopH);
+
+        mFrameBufferBatch.end();
+        mFrameBuffer.end();
+    }
+
+    private void drawFrameBuffer(Batch batch, float offset) {
+        final float U = Constants.UNIT_FOR_PIXEL;
+        int w = mFrameBuffer.getWidth();
+        int h = mFrameBuffer.getHeight();
+        Texture texture = mFrameBuffer.getColorBufferTexture();
+        batch.draw(texture,
+                // dst
+                getX() - w * U / 2 + offset, getY() - BODY_CENTER.y * U - offset, w * U, h * U,
+                // src
+                0, 0, w, h,
+                // flips
+                false, true);
     }
 
     @Override
