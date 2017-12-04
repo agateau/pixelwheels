b382cf0bf71ac270742684b526fbeef776c3b9d7
Aurélien Gâteau <mail@agateau.com>
2017-12-04 08:09:09 +0100

    Hide navigation buttons in Android

--- android/src/com/agateau/tinywheels/android/AndroidLauncher.java
+++ android/src/com/agateau/tinywheels/android/AndroidLauncher.java
@@ -30,6 +30,8 @@ public class AndroidLauncher extends AndroidApplication {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useImmersiveMode = true;
+        config.hideStatusBar = true;
         FileUtils.appName = "tinywheels";
         initialize(new TwGame(), config);
     }
