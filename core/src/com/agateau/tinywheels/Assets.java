f8ce3ccc103de06ced03f3078eebd99cda416c4e
Aurélien Gâteau <mail@agateau.com>
2017-12-26 20:39:48 +0100

    Larger font for rank hud

--- core/src/com/agateau/tinywheels/Assets.java
+++ core/src/com/agateau/tinywheels/Assets.java
@@ -61,6 +61,17 @@ public class Assets {
             parameter.size = 12;
             parameter.borderWidth = 0.5f;
             this.skin.add("small-font", loadFont("fonts/Xolonium-Regular.ttf", parameter));
+
+            parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+            parameter.size = 28;
+            parameter.borderWidth = 0.5f;
+            this.skin.add("hud-font", loadFont("fonts/Xolonium-Regular.ttf", parameter));
+
+            parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+            parameter.size = 56;
+            parameter.characters = "1234567890thsrdneméè";
+            parameter.borderWidth = 0.5f;
+            this.skin.add("hud-rank-font", loadFont("fonts/Xolonium-Regular.ttf", parameter));
         }
 
         private BitmapFont loadFont(String name, int size) {
@@ -68,6 +79,7 @@ public class Assets {
             parameter.size = size;
             return loadFont(name, parameter);
         }
+
         private BitmapFont loadFont(String name, FreeTypeFontGenerator.FreeTypeFontParameter parameter) {
             FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(name));
             BitmapFont font = generator.generateFont(parameter);
