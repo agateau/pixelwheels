a934e165493e76b04f23d7b08fcb194edd113954
Aurélien Gâteau <mail@agateau.com>
2017-12-27 12:37:34 +0100

    Unbreak gallery

--- uigallery/src/com/agateau/ui/gallery/GalleryScreen.java
+++ uigallery/src/com/agateau/ui/gallery/GalleryScreen.java
@@ -29,8 +29,10 @@ import com.agateau.ui.anchor.SizeRule;
 import com.agateau.utils.log.NLog;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Scaling;
@@ -54,7 +56,44 @@ class GalleryScreen extends StageScreen {
 
     private void loadSkin() {
         mAtlas = new TextureAtlas(Gdx.files.internal("ui/uiskin.atlas"));
-        mSkin = new Skin(Gdx.files.internal("ui/uiskin.json"), mAtlas);
+        mSkin = new Skin(mAtlas);
+        loadFonts();
+        mSkin.load(Gdx.files.internal("ui/uiskin.json"));
+    }
+
+    private void loadFonts() {
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+        mSkin.add("default-font", loadFont("fonts/Xolonium-Regular.ttf", 28));
+        mSkin.add("title-font", loadFont("fonts/Aero.ttf", 32));
+
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 12;
+        parameter.borderWidth = 0.5f;
+        mSkin.add("small-font", loadFont("fonts/Xolonium-Regular.ttf", parameter));
+
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 28;
+        parameter.borderWidth = 0.5f;
+        mSkin.add("hud-font", loadFont("fonts/Xolonium-Regular.ttf", parameter));
+
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 56;
+        parameter.characters = "1234567890thsrdneméè";
+        parameter.borderWidth = 0.5f;
+        mSkin.add("hud-rank-font", loadFont("fonts/Xolonium-Regular.ttf", parameter));
+    }
+
+    private BitmapFont loadFont(String name, int size) {
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = size;
+        return loadFont(name, parameter);
+    }
+
+    private BitmapFont loadFont(String name, FreeTypeFontGenerator.FreeTypeFontParameter parameter) {
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(name));
+        BitmapFont font = generator.generateFont(parameter);
+        generator.dispose();
+        return font;
     }
 
     private void setupUi() {
