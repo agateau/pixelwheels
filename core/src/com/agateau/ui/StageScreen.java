f2b6ab7a676618e59d1368292d7a9ea6c11db0a0
Aurelien Gateau <mail@agateau.com>
2018-05-01 11:11:13 +0200

    Import cleanup

--- core/src/com/agateau/ui/StageScreen.java
+++ core/src/com/agateau/ui/StageScreen.java
@@ -19,7 +19,6 @@
 package com.agateau.ui;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
