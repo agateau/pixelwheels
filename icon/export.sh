66f7e750d688cb1d3dbef1fb03635bfb8122373e
Aurelien Gateau <mail@agateau.com>
2019-08-11 12:43:22 +0200

    Generate Google Play icon from icon/

--- icon/export.sh
+++ icon/export.sh
@@ -7,6 +7,10 @@ ANDROID_DIR=$PWD/../android
 ASSETS_DIR=$ANDROID_DIR/assets
 RES_DIR=$ANDROID_DIR/res
 
+GPLAY_ICON=$PWD/../fastlane/metadata/android/en-US/images/icon.png
+
+BGCOLOR=#5a6988
+
 # mdpi & xhdpi
 aseprite --batch icon.ase --slice icon-48 \
     --save-as $RES_DIR/drawable-mdpi/ic_launcher.png \
@@ -20,3 +24,8 @@ aseprite --batch icon.ase --slice icon-72 \
 # Desktop
 aseprite --batch icon.ase --slice icon-72 \
     --scale 2 --save-as $ASSETS_DIR/desktop-icon/desktop-icon.png
+
+# Google Play: 512 x 512 RVB, flat
+# (Start from the biggest, unscaled version)
+convert -scale 512x512 $RES_DIR/drawable-hdpi/ic_launcher.png \
+    -background "$BGCOLOR" -flatten $GPLAY_ICON
