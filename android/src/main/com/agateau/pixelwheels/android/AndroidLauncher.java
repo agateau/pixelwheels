4f4bfb760cee3673303fd562903cdbb3198b33b3
Aurelien Gateau <mail@agateau.com>
2021-12-23 12:46:46 +0100

    Fix Android 11 statusbar not disappearing when switching between apps
    Fixes #140

--- android/src/main/com/agateau/pixelwheels/android/AndroidLauncher.java
+++ android/src/main/com/agateau/pixelwheels/android/AndroidLauncher.java
@@ -20,6 +20,7 @@ package com.agateau.pixelwheels.android;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import com.agateau.pixelwheels.Constants;
 import com.agateau.pixelwheels.PwGame;
 import com.agateau.pixelwheels.utils.StringUtils;
@@ -46,6 +47,27 @@ public class AndroidLauncher extends AndroidApplication {
         setupLogging(game);
         initialize(game, config);
         Gdx.input.setCatchBackKey(true);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // Work around for the status-bar coming back when switching between apps
+        // on Android 11.
+        // https://github.com/agateau/pixelwheels/issues/140
+        // This code comes from:
+        // https://github.com/libgdx/libgdx/issues/6006#issuecomment-619967289
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow()
+                    .getDecorView()
+                    .setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        }
     }
 
     private void setupLogging(PwGame game) {
