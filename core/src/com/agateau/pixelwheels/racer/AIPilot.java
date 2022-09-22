cab657a73586473824092264452b527317a889e1
Aurelien Gateau <mail@agateau.com>
2022-09-22 12:46:26 +0200

    Modernize AIPilot
    - Use function reference instead of a class
    - Replace deprecated function calls

--- core/src/com/agateau/pixelwheels/racer/AIPilot.java
+++ core/src/com/agateau/pixelwheels/racer/AIPilot.java
@@ -43,13 +43,6 @@ public class AIPilot implements Pilot {
     // How much of the vehicle width to move the target to avoid a mine
     private static final float MINE_AVOIDANCE_FACTOR = 2;
 
-    class MineFilter implements ClosestBodyFinder.BodyFilter {
-        @Override
-        public boolean acceptBody(Body body) {
-            return BodyIdentifier.isStaticObstacle(body);
-        }
-    }
-
     private enum State {
         NORMAL,
         BLOCKED,
@@ -80,8 +73,7 @@ public class AIPilot implements Pilot {
     private final Track mTrack;
     private final Racer mRacer;
 
-    private final MineFilter mMineFilter = new MineFilter();
-    private final ClosestBodyFinder mClosestBodyFinder = new ClosestBodyFinder(mMineFilter);
+    private final ClosestBodyFinder mClosestBodyFinder = new ClosestBodyFinder(BodyIdentifier::isStaticObstacle);
 
     private State mState = State.NORMAL;
     private float mBlockedDuration = 0;
@@ -209,7 +201,7 @@ public class AIPilot implements Pilot {
             switchToBlocked();
             return;
         }
-        float targetAngle = mTmpVector1.set(target.position).sub(mRacer.getPosition()).angle();
+        float targetAngle = mTmpVector1.set(target.position).sub(mRacer.getPosition()).angleDeg();
         targetAngle = AgcMathUtils.normalizeAngle(targetAngle);
 
         Vehicle vehicle = mRacer.getVehicle();
@@ -253,7 +245,7 @@ public class AIPilot implements Pilot {
         Vector2 adjustedTargetPos = mTmpVector3;
 
         Vehicle vehicle = mRacer.getVehicle();
-        halfWidth.set(0, vehicle.getHeight() / 2).rotate(vehicle.getAngle());
+        halfWidth.set(0, vehicle.getHeight() / 2).rotateDeg(vehicle.getAngle());
 
         // Check on the right
         position.set(mRacer.getPosition()).add(halfWidth);
