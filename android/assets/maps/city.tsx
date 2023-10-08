5eeca8b43e2c991161ac99cd949e7095599bfe5e
Aurelien Gateau <mail@agateau.com>
2023-10-08 20:53:11 +0200

    Tiny-sur-Mer: improve lighthouse

--- android/assets/maps/city.tsx
+++ android/assets/maps/city.tsx
@@ -1,9 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="city" tilewidth="64" tileheight="64" spacing="4" margin="2" tilecount="170" columns="10">
+<tileset version="1.10" tiledversion="1.10.2" name="city" tilewidth="64" tileheight="64" spacing="4" margin="2" tilecount="204" columns="10">
  <image source="city.png" width="680" height="1156"/>
- <terraintypes>
-  <terrain name="Roof" tile="-1"/>
- </terraintypes>
  <tile id="5">
   <properties>
    <property name="start" value="true"/>
@@ -29,10 +26,6 @@
    <property name="material" value="SAND"/>
   </properties>
  </tile>
- <tile id="40" terrain="0,,,"/>
- <tile id="42" terrain=",0,,"/>
- <tile id="50" terrain=",,0,"/>
- <tile id="52" terrain=",,,0"/>
  <tile id="80">
   <properties>
    <property name="material" value="WATER"/>
@@ -108,4 +101,13 @@
    <property name="material" value="WATER"/>
   </properties>
  </tile>
+ <wangsets>
+  <wangset name="Terrains" type="corner" tile="-1">
+   <wangcolor name="Roof" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="40" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="42" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="50" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="52" wangid="0,0,0,1,0,0,0,0"/>
+  </wangset>
+ </wangsets>
 </tileset>
